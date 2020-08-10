fd8e78c35ca56a8ddbcb96b9b30777df343d017b
Ryan Croxford <ryan.croxford@uvm.edu>
2020-08-10 15:37:47 -0400

    Updates

---
+++ precache-manifest.dfeb40b973fb03a137933dfe2f2acb2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4af862e80607c49d28b9fcf68030bf",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "01270ede781688db73f3",
+    "url": "/portfolio-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76a72856b13dac16c0b3",
+    "url": "/portfolio-app/static/js/2.c00a4803.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-app/static/js/2.c00a4803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01270ede781688db73f3",
+    "url": "/portfolio-app/static/js/main.b9164293.chunk.js"
+  },
+  {
+    "revision": "71498c21d321f5d1c918",
+    "url": "/portfolio-app/static/js/runtime-main.34a8331a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
