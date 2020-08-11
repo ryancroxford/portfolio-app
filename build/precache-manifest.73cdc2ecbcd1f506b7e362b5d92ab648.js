0c940e0989d08ebc7a37691ce56bce488ac2b23c
Ryan Croxford <ryan.croxford@uvm.edu>
2020-08-10 22:00:23 -0400

    change homepage declaration in package.json

---
+++ build/precache-manifest.73cdc2ecbcd1f506b7e362b5d92ab648.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4b7478617f0c5a4300e6ba1c3980cf",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "c0242839dda31a6d31b2",
+    "url": "/portfolio-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76a72856b13dac16c0b3",
+    "url": "/portfolio-app/static/js/2.c00a4803.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-app/static/js/2.c00a4803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0242839dda31a6d31b2",
+    "url": "/portfolio-app/static/js/main.b27809ff.chunk.js"
+  },
+  {
+    "revision": "71498c21d321f5d1c918",
+    "url": "/portfolio-app/static/js/runtime-main.34a8331a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
